c8c961ed72207475efbe84db3952ad2df43adb48
cfredric <chris.p.fredrickson@gmail.com>
2015-09-02 00:10:13 -0400

    038 in Haskell

--- 038/038.hs
+++ 038/038.hs
@@ -1,9 +1,25 @@
---prob038
+import Data.Char
 
+prob038 = maximum $ map read (map listToString productsList) :: Int
+    where
+    productsList = filter isPanDigital [buildConcatenatedProduct k 9 | k <- [1..9999]]
+
+buildConcatenatedProduct :: Int -> Int -> [Int]
+buildConcatenatedProduct k lengthLimit = concatHelper k lengthLimit [] [1..]
+    where
+    concatHelper :: Int -> Int -> [Int] -> [Int] -> [Int]
+    concatHelper k lengthLimit acc (n:ns)
+        | length acc > lengthLimit = []
+        | length acc == lengthLimit = if n == 2 then [] else acc
+        | otherwise = concatHelper k lengthLimit (acc ++ (map digitToInt $ show $ k*n)) ns
+
+listToString :: [Int] -> [Char]
+listToString [] = "0"
+listToString ns = map intToDigit ns
 
 isPanDigital :: [Int] -> Bool
 isPanDigital list = length list == 9 && and (listApply (map elem [1..9]) list)
-
-listApply :: [[a] -> b] -> [a] -> [b]
-listApply [] _ = []
-listApply (f:fs) list = (f list) : listApply fs list
+    where
+    listApply :: [[a] -> b] -> [a] -> [b]
+    listApply [] _ = []
+    listApply (f:fs) list = (f list) : listApply fs list
