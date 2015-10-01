957ce71072725ce0f2844f5f3b230aa4d2564c52
cfredric <chris.p.fredrickson@gmail.com>
2015-09-30 20:11:03 -0400

    use a set for speed

--- 050/050.hs
+++ 050/050.hs
@@ -1,5 +1,6 @@
 import Control.Parallel.Strategies (using, parList, rdeepseq)
-import Data.Numbers.Primes (isPrime, primes)
+import Data.Set (fromList, toList, member)
+import Data.Numbers.Primes (primes)
 
 main = print prob050
 
@@ -7,18 +8,14 @@ prob050 = findLongestPrimeSequenceUnderLimit 1000000 2
 
 findLongestPrimeSequenceUnderLimit :: Int -> Int -> (Int, Int)
 findLongestPrimeSequenceUnderLimit upperLim maxStart = maximum (tuples `using` parList rdeepseq)
-    where tuples = map (findLongestPrimeSequenceFromStart upperLim) $ boundPrimes maxStart upperLim
-
-findLongestPrimeSequenceFromStart :: Int -> Int -> (Int, Int)
-findLongestPrimeSequenceFromStart upperLim start = maximum parSums
     where
-    primeList = dropWhile (< start) $ uBoundPrimes upperLim
-    consecutiveSums = scanl (\(count, sum) p -> (count+1, sum+p)) (0, 0) primeList
-    sumsUnderLimit = takeWhile (\(count, sum) -> sum < upperLim) consecutiveSums
-    primeSums = filter (\(count, sum) -> isPrime sum) sumsUnderLimit
-    parSums = primeSums `using` parList rdeepseq
-
-boundPrimes :: Int -> Int -> [Int]
-boundPrimes low high = dropWhile (< low) $ uBoundPrimes high
-
-uBoundPrimes high = takeWhile (< high) primes
+    tuples = map findLongestPrimeSequenceFromStart primeList
+    primeSet = fromList $ dropWhile (< maxStart) $ takeWhile (< upperLim) primes
+    primeList = toList primeSet
+    findLongestPrimeSequenceFromStart :: Int -> (Int, Int)
+    findLongestPrimeSequenceFromStart start = maximum parSums
+        where
+        consecutiveSums = scanl (\(count, sum) p -> (count+1, sum+p)) (0, 0) $ dropWhile (< start) primeList
+        sumsUnderLimit = takeWhile (\(count, sum) -> sum < upperLim) consecutiveSums
+        primeSums = filter (\(count, sum) -> member sum primeSet) sumsUnderLimit
+        parSums = primeSums `using` parList rdeepseq
