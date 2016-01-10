7414211dad733b9efabc7621874fbcbac4711b99
cfredric <chris.p.fredrickson@gmail.com>
2016-01-09 23:22:41 -0500

    rewrite 036 haskell

--- 036/036.hs
+++ 036/036.hs
@@ -1,9 +1,16 @@
+module E036 where
+
 import Data.Char (intToDigit)
 import Numeric (showIntAtBase)
+
+prob036 :: Int
 prob036 = sum [x | x <- [1..1000000], isBinaryPalindrome x, isDecimalPalindrome x]
 
 isBinaryPalindrome :: Int -> Bool
-isBinaryPalindrome n = let str = (showIntAtBase 2 intToDigit n) "" in str == reverse str
+isBinaryPalindrome n = isPalindrome $ (showIntAtBase 2 intToDigit n) ""
 
 isDecimalPalindrome :: Int -> Bool
-isDecimalPalindrome n = let str = show n in str == reverse str
+isDecimalPalindrome n = isPalindrome $ show n
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = reverse xs == xs
